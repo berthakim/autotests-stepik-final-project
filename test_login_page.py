e80d089a6e7f6955cdf8a5c22d4396d743605cdf
berthakim <kte.ent@gmail.com>
2021-05-04 23:23:15 +0900

    Implemented LoginPage class along with its testing methods

---
+++ test_login_page.py
@@ -0,0 +1,20 @@
+from .pages.login_page import LoginPage
+
+
+def test_guest_is_on_login_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+	page = LoginPage(browser, link)
+	page.open()
+	page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_registration_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
--- test_main_page.py
+++ test_main_page.py
@@ -10,8 +10,7 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 def test_gest_should_see_login_link(browser):
-	link = "http://selenium1py.pythonanywhere.com/"
-	page = MainPage(browser, link)
-	page.open()
-	page.should_be_login_link()
-    
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
