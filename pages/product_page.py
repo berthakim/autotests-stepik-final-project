de3f39f6ecafcdfc8b54239252cfa9499df5fa83
berthakim <kte.ent@gmail.com>
2021-05-06 17:35:11 +0900

    Multiple links check using 'parametrize' and 'marks' - looking for a bug

--- pages/product_page.py
+++ pages/product_page.py
@@ -23,7 +23,8 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name isn't present on page"
         product_name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE_SUCCES).text
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        assert product_name_in_message == product_name, "Product wasn't added (name of product is not in message)"
+        assert product_name_in_message == product_name, "Name of product in message doesn't match \
+                                                         with name product that we added to basket"
 
     def should_be_price_of_cart(self):
         assert self.is_element_present(*ProductPageLocators.PRICE_IN_MESSAGE), "No message of price"
@@ -32,4 +33,4 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.PRICE_OF_PRODUCT), "Price of product isn't correct"
         price_message = self.browser.find_element(*ProductPageLocators.PRICE_IN_MESSAGE).text
         price_product = self.browser.find_element(*ProductPageLocators.PRICE_OF_PRODUCT).text
-        assert price_message == price_message, "Price in message doesn't equalt to price of product"
+        assert price_message == price_message, "Price in message doesn't equal to price of product"
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,27 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/fr/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+# def test_guest_can_add_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/fr/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_article_to_cart()
+
+# def test_guest_can_add_product2_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_article_to_cart()
+
+urls_common_base = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+# 7th url contain an error which will not supposed to be debugged
+urls_list = [f'{urls_common_base}{n}' for n in range(10) if n != 7]
+
+@pytest.mark.parametrize('urls_list', [*urls_list, 
+	                                  pytest.param(f'{urls_common_base}7', marks=pytest.mark.xfail)]) 
+def test_guest_can_add_product_to_basket(browser, urls_list):
+    print(urls_list)
+    page = ProductPage(browser, urls_list)
     page.open()
     page.add_article_to_cart()
